54dc0c2d8ec7372860b8d50e3a582077a2c045fa
Michael <xiaoyu.tamu@gmail.com>
2022-02-28 11:22:22 -0600

    feat(*): initialize

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import("prettier").Options}
+ */
+module.exports = {
+  arrowParens: 'always',
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  quoteProps: 'consistent',
+};
